4c199845605b40da20223ccd492c193b44c03b43
s.krasko <s.krasko@notamedia.com.ru>
2024-02-16 17:10:47 +0300

    обнова

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import React, { useEffect, useState } from "react";
-import Cards from "./components/Contents/Cards";
-import Navigation from "./components/UI/Navigation";
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from 'react'
+import Cards from './components/Contents/Cards'
+import Navigation from './components/UI/Navigation'
 
 const App = () => {
-  const [address, setAddress] = useState('https://rickandmortyapi.com/api/character');
-  const [infoObject, setInfoObject] = useState({info: {}, results: []});
+  const [address, setAddress] = useState('https://rickandmortyapi.com/api/character')
+  const [infoObject, setInfoObject] = useState({ info: {}, results: [] })
 
   useEffect(() => {
     fetch(address)
       .then((response) => {
-        return response.json();
+        return response.json()
       })
-      .then((data) => {        
+      .then((data) => {
         setInfoObject(data)
       })
-  }, [address]);
-  
-  console.log(infoObject);
+  }, [address])
+
+  console.log(infoObject)
 
   return (
     <section>
       <Cards cardsArr={infoObject.results} />
       <Navigation object={infoObject} changeAdr={setAddress} />
     </section>
-  );
-};
+  )
+}
 
-export default App;
+export default App
