7461499d7393ace87b67c4aa9b2d790e6c6ced54
s.krasko <s.krasko@notamedia.com.ru>
2024-03-04 20:09:55 +0300

    помог Арс

--- src/components/UI/Pagination.jsx
+++ src/components/UI/Pagination.jsx
@@ -1,29 +1,5 @@
 import React, { useState } from 'react'
 import styles from './Pagination.module.css'
-import { returnPaginationRange } from '../../utils/appUtils'
-
-// <?php
-// $currentPage = (int)$_GET['page'];
-// $startPage = 1;
-// $endPage = 56;
-// $rangePage = 1;
-// for ($i = $startPage; $i <= $endPage; $i++) {
-//     if ($i === $startPage) {
-//         echo '<a href="?page='.$i.'">'.$startPage.'</a>';
-//     } elseif($i === $currentPage) {
-//         echo '<a href="?page='.$i.'">'."<b>$i</b>".'</a>';
-//     } elseif ($i === $currentPage - ($rangePage+1) || $i === $currentPage + ($rangePage+1)) {
-//         echo '<a href="?page='.$i.'">'.'...'.'</a>';
-//     } elseif ($i - $rangePage <= $currentPage && $currentPage <= $i +$rangePage) {
-//         echo '<a href="?page='.$i.'">'.$i.'</a>';
-//     } elseif($i === $endPage) {
-//         echo '<a href="?page='.$i.'">'.$endPage.'</a>';
-//         continue;
-//     } else {
-//         continue;
-//     }
-//     echo ' --- ';
-// }
 
 const Pagination = (props) => {
   const [currentPage, setCurrentPage] = useState(1)
--- src/utils/appUtils.js
+++
@@ -1,9 +0,0 @@
-export const returnPaginationRange = (page, limit) => {
-  const array = [];
-
-  for (let i = 0; i < limit; i++) {
-    array.push(i + 1)
-  }
-
-  return array;
-}
